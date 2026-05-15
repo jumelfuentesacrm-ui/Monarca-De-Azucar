9d12a953acb215f3f8d4ee42a7407a0a906e99df
jumelfuentesacrm-ui <jumelfuentesacrm@gmail.com>
2026-05-15 19:59:44 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico"/>
         <link rel="manifest" href="/manifest.json"/>
         <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
         <meta name="apple-mobile-web-app-title" content="Monarca de Azúcar"/>
